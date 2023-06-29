322e70641a31f3c40f909c5dacbc83850fe36884
Kelvin Omozokpia <kelvinjune2@gmail.com>
2023-06-29 12:50:17 +0100

    React native playground

---
+++ App.tsx
@@ -0,0 +1,36 @@
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput placeholder="Your course goal!" style={styles.textInput} />
+        <Button title="Add Goal" />
+      </View>
+      <View>
+        <Text>List of goals... </Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  appContainer: {
+    padding: 50,
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: '#cccccc',
+    flex: 1,
+    marginRight: 8,
+    padding: 8,
+  },
+  // button: {
+  //   padding: 8,
+  // },
+});
